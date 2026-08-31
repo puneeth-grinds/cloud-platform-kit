8129d42a0e546f5cc623c95c2d2a98295db584c5
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 12:07:53 +0530

    setup

--- terraform/state-backend/variables.tf
+++ terraform/state-backend/variables.tf
@@ -1,14 +1,16 @@
+# AWS account ID
 variable "aws_account_id" {
   type        = string
   description = "Personal AWS account ID"
 }
 
+# AWS Default Region
 variable "aws_region" {
   type        = string
   description = "Specifies the AWS region"
   default     = "us-west-1"
 }
-
+# Project name
 variable "project" {
   type        = string
   description = "Specifies the project name"
