dc64c3152e72fe8fbe8ec93852b82345dbd336bc
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 10:42:05 +0530

    setup

--- terraform/state-backend/versions.tf
+++ terraform/state-backend/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.16.0"
+  required_version = ">=1.10.0"
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>5.52.0"
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
   }
 }
