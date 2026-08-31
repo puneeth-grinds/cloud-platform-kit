bb761228555e989447e44dde229a58e7f890e17d
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 11:37:44 +0530

    setup

--- terraform/state-backend/main.tf
+++ terraform/state-backend/main.tf
@@ -1,7 +1,9 @@
+# Create S3 bucket
 resource "aws_s3_bucket" "state_backend_tf" {
   bucket = "state-backend-tf"
 }
 
+# Enable versioning 
 resource "aws_s3_bucket_versioning" "state_backend_tf_versioning" {
   bucket = aws_s3_bucket.state_backend_tf.id
 
@@ -9,4 +11,15 @@ resource "aws_s3_bucket_versioning" "state_backend_tf_versioning" {
     status = "Enabled"
   }
 
+}
+
+# Server-Side Encryption 
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_backend_tf_encryption" {
+  bucket = aws_s3_bucket_versioning.state_backend_tf_versioning.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
