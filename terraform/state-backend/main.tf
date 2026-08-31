165b7f46851bd78e7a75ea7cda0380c2227055b3
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 11:34:23 +0530

    setup

--- terraform/state-backend/main.tf
+++ terraform/state-backend/main.tf
@@ -1,3 +1,12 @@
-resource "aws_s3_bucket" "state-backend-tf" {
+resource "aws_s3_bucket" "state_backend_tf" {
   bucket = "state-backend-tf"
+}
+
+resource "aws_s3_bucket_versioning" "state_backend_tf_versioning" {
+  bucket = aws_s3_bucket.state_backend_tf.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
 }
