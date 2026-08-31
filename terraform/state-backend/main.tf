602f3a36ca95f10e0d68141a109c199b3a0cc5a8
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 11:40:50 +0530

    setup

--- terraform/state-backend/main.tf
+++ terraform/state-backend/main.tf
@@ -15,11 +15,22 @@ resource "aws_s3_bucket_versioning" "state_backend_tf_versioning" {
 
 # Server-Side Encryption 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state_backend_tf_encryption" {
-  bucket = aws_s3_bucket_versioning.state_backend_tf_versioning.id
+  bucket = aws_s3_bucket.state_backend_tf.id
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
+}
+
+# Enable Public Access Protection
+resource "aws_s3_bucket_public_access_block" "state_backend_tf_public_block" {
+  bucket = aws_s3_bucket.state_backend_tf.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
 }
