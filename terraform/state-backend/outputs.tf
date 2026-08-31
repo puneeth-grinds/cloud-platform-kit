d7dd7a2f51b200f4db812a615e14e3aa41324c9b
puneeth-grinds <puneethm2736@gmail.com>
2026-08-31 11:59:58 +0530

    setup

---
+++ terraform/state-backend/outputs.tf
@@ -0,0 +1,6 @@
+# Backend state S3 bucket
+output "state_bucket_name" {
+  description = "Gives the bucket name on which tf state file is stored"
+  value       = aws_s3_bucket.state_backend_tf
+  sensitive   = false
+}
