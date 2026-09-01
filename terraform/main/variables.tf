cfb2388fb9b01b887483c62f972c37acb8d6ef2c
puneeth-grinds <puneethm2736@gmail.com>
2026-09-01 09:41:33 +0530

    terraform

---
+++ terraform/main/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type = string
+  description = "Specifies the AWS region"
+  default = "us-west-1"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "Personal AWS account ID"
+  default     = "512297269123"
+}
