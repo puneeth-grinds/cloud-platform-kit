471d8fbc9bcab4666853cb97b30821a4727ab468
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 10:26:01 +0530

    readme

--- terraform/main/endpoints.tf
+++ terraform/main/endpoints.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "s3_gateway_endpoint" {
-  region = var.aws_region
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.${var.aws_region}.s3"
-  route_table_ids = aws_route_table.private-route-table
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private-route-table.id]
 }
