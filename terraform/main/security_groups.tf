3e5d995d68a62de9021bc6e445d0003d167b420d
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 12:21:41 +0530

    readme

--- terraform/main/security_groups.tf
+++ terraform/main/security_groups.tf
@@ -4,3 +4,10 @@ resource "aws_security_group" "alb_sg" {
   description = "Allow all traffic from internet on port 80"
   vpc_id      = aws_vpc.vpc.id
 }
+
+resource "aws_vpc_security_group_ingress_rule" "alb_sg_ingress_rule" {
+  security_group_id = aws_security_group.alb_sg
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
