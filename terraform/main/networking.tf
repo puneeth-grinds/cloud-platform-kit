38693ed730f62ab1873b26fc8a71cf317da57c53
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 09:21:55 +0530

    readme

--- terraform/main/networking.tf
+++ terraform/main/networking.tf
@@ -75,12 +75,6 @@ resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
-
-  }
-
-  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gateway.id
   }
@@ -93,11 +87,6 @@ resource "aws_route_table" "public-route-table" {
 
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "10.0.0.0/16"
-    nat_gateway_id = "local"
-  }
 
   route {
     cidr_block     = "0.0.0.0/0"
