a4274805ec6f028aaaac5fbdbc9f678fad0c3cc5
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 12:41:45 +0530

    readme

--- terraform/main/security_groups.tf
+++ terraform/main/security_groups.tf
@@ -6,22 +6,30 @@ resource "aws_security_group" "alb_sg" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name = "allow alb to reach on port 8080"
+  name        = "allow alb to reach on port 8080"
   description = "Allow the ALB to reach the ECS application"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.vpc.id
 }
 resource "aws_vpc_security_group_ingress_rule" "alb_sg_ingress_rule" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
-  ip_protocol       = "tcp"
   to_port           = 80
-}
+  ip_protocol       = "tcp"
 
+}
+resource "aws_vpc_security_group_egress_rule" "alb_sg_ingress_rule" {
+  security_group_id = aws_security_group.alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
 resource "aws_vpc_security_group_egress_rule" "ecs_sg_ingress_rule" {
   security_group_id = aws_security_group.ecs_sg
-  from_port = 8080
-  to_port = 8080
-  ip_protocol = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
 
 }
+
