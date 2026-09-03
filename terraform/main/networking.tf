fcac5430c625e97c6bd01fa64bfbae9b3bb3f4d3
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 07:52:44 +0530

    readme

--- terraform/main/networking.tf
+++ terraform/main/networking.tf
@@ -59,3 +59,13 @@ resource "aws_eip" "elastic_ip" {
     Name = "Elastic-IP"
   }
 }
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id = var.public_1a.id
+  depends_on = [ aws_internet_gateway.gateway.id ]
+  
+  tags = {
+    Name = "Nat-Gateway"
+  }
+}
