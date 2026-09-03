848d1de78d1981fed2a55000ab2ba6ca7807693c
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 10:33:41 +0530

    readme

--- terraform/main/endpoints.tf
+++ terraform/main/endpoints.tf
@@ -4,3 +4,12 @@ resource "aws_vpc_endpoint" "s3_gateway_endpoint" {
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.private-route-table.id]
 }
+
+resource "aws_vpc_endpoint" "ecr_gateway_endpoint" {
+  vpc_id              = aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_1a.id, aws_subnet.private_1b.id]
+  private_dns_enabled = true
+
+}
