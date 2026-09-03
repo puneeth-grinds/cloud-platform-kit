04d3ce8a451b088b7c08a0a49820548e85e5fd58
puneeth-grinds <puneethm2736@gmail.com>
2026-09-03 13:10:07 +0530

    readme

--- terraform/main/security_groups.tf
+++ terraform/main/security_groups.tf
@@ -57,9 +57,17 @@ resource "aws_vpc_security_group_egress_rule" "ecs_sg_egress_rds" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "ecs_sg_egress_anywhere" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_sg_ingress" {
   security_group_id            = aws_security_group.rds_sg.id
-  referenced_security_group_id = aws_security_group.rds_sg.id
-  from_port                    = 0
-  to_port                      = 0
-  ip_protocol                  = -1
+  referenced_security_group_id = aws_security_group.ecs_sg.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
 }
