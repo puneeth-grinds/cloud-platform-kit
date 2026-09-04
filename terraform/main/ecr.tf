a41f0cacc285e400c315c7f56db1d5424414c9a5
puneeth-grinds <puneethm2736@gmail.com>
2026-09-04 12:20:01 +0530

    readme

--- terraform/main/ecr.tf
+++ terraform/main/ecr.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "ecr" {
+resource "aws_ecr_repository" "ecr_api_gateway" {
   name                 = "ecr for api gateway"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "ecr_vulnerability_scanner" {
+  name                 = "ecr for vulnerability scanner"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
