a19e9c2fbcde1074155b0931460f7fca94c20af6
puneeth-grinds <puneethm2736@gmail.com>
2026-09-04 12:33:55 +0530

    readme

--- terraform/main/ecr.tf
+++ terraform/main/ecr.tf
@@ -15,3 +15,28 @@ resource "aws_ecr_repository" "ecr_vulnerability_scanner" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy_api_gateway" {
+  repository = aws_ecr_repository.ecr_api_gateway.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire images older than 1 days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+
+}
