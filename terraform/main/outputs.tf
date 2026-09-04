2129f68e13b635bee36f26a8dae934b8a14c4e37
puneeth-grinds <puneethm2736@gmail.com>
2026-09-04 12:56:39 +0530

    readme

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -1,5 +1,9 @@
 output "ecr_api_gateway_url" {
-  value = aws_ecr_repository.ecr_api_gateway.repository_url
+  value       = aws_ecr_repository.ecr_api_gateway.repository_url
   description = "ECR Repository URL for api gateway"
 }
 
+output "ecr_vulnerability_scanner_url" {
+  value       = aws_ecr_repository.ecr_vulnerability_scanner.repository_url
+  description = "ECR Repository URL for Vulnerability Scanner"
+}
