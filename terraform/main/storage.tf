825bcb27b4f6fbba109fe071e797453fa4416eeb
puneeth-grinds <puneethm2736@gmail.com>
2026-09-06 09:00:33 +0530

    readme

--- terraform/main/storage.tf
+++ terraform/main/storage.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "s3_storage" {
   bucket = "cloud-platform-kit-app-=${var.aws_account_id}"
+  tags = {
+    Name = "cloud-platform-kit-storage-bucket"
+  }
+}
 
+resource "aws_s3_bucket_versioning" "s3_storage_versioning" {
+  bucket = aws_s3_bucket.s3_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
