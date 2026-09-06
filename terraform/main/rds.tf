cfe11143e96b3d75777f01a1103576792327abcb
puneeth-grinds <puneethm2736@gmail.com>
2026-09-06 08:04:42 +0530

    readme

--- terraform/main/rds.tf
+++ terraform/main/rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 }
 
 resource "aws_db_instance" "RDS_DB" {
-  engine                 = "postgres"
+  engine                 = "PostgreSQL"
   engine_version         = "16"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
