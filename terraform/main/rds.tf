a3e48827f215ab699ded2af40c8b012f70494905
puneeth-grinds <puneethm2736@gmail.com>
2026-09-06 07:25:47 +0530

    readme

--- terraform/main/rds.tf
+++ terraform/main/rds.tf
@@ -9,13 +9,16 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 }
 
 resource "aws_db_instance" "RDS_DB" {
-  engine            = "PostgreSQL"
-  engine_version    = "16"
-  instance_class    = "db.t3.micro"
-  allocated_storage = "20GB"
-  multi_az          = false
-  availability_zone = var.availability_zone_1a
-  db_name           = var.rds_database_name
-  username          = var.rds_database_name
-  password          = var.rds_database_password
+  engine              = "PostgreSQL"
+  engine_version      = "16"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = "20GB"
+  multi_az            = false
+  availability_zone   = var.availability_zone_1a
+  db_name             = var.rds_database_name
+  username            = var.rds_database_name
+  password            = var.rds_database_password
+  publicly_accessible = false
+  skip_final_snapshot = true
+  deletion_protection = false
 }
