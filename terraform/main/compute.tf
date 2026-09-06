4843e8744c681dd7e7f8ca8558874c37ef6cb0f3
puneeth-grinds <puneethm2736@gmail.com>
2026-09-06 09:48:38 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -2,4 +2,6 @@ resource "aws_lb" "lb" {
   name = "app_lb"
   internal = false
   load_balancer_type = "application"
+  subnets = [ aws_subnet.public_1a, aws_subnet.private_1b ]
+  security_groups = [ aws_security_group.alb_sg.id ]
 }
