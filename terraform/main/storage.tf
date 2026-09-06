e5133caa5fed989833ea3992634d15326720a15e
puneeth-grinds <puneethm2736@gmail.com>
2026-09-06 09:04:58 +0530

    readme

--- terraform/main/storage.tf
+++ terraform/main/storage.tf
@@ -21,3 +21,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_storage_sse" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "se_storage_public_access_block" {
+  bucket = aws_s3_bucket.s3_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
