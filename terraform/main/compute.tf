1f56dd8131d06cc00fa13efbfafe51aee50c5f54
puneeth-grinds <puneethm2736@gmail.com>
2026-09-07 08:27:35 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -137,6 +137,18 @@ resource "aws_ecs_task_definition" "ecs_task_vulscanner" {
           protocol      = "tcp"
         }
       ]
+      secrets = [
+        {
+          name      = "DATABASE_URL"
+          valueFrom = aws_ssm_parameter.rds_db_connection_string.arn
+        }
+      ]
+      environment = [
+        {
+          name  = "S3_BUCKET"
+          value = "http://vulnerability-scanner.cloud-platform-kit.local:8081"
+        }
+      ]
     }
   ]
 }
