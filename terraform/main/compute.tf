643436bb73160a4ec5d20afe867dae9c86fa5eeb
puneeth-grinds <puneethm2736@gmail.com>
2026-09-07 07:22:26 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -1,20 +1,20 @@
 resource "aws_lb" "lb" {
-  name               = "app_lb"
+  name               = "cloudplatformkitlb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.public_1a, aws_subnet.private_1b]
+  subnets            = [aws_subnet.public_1a.id, aws_subnet.private_1b.id]
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
 resource "aws_alb_target_group" "api_alb_target" {
-  name = "api-gateway-target-group"
-  vpc_id = aws_vpc.vpc.id
-  port = 8080
-  protocol = "HTTP"
+  name        = "api-gateway-target-group"
+  vpc_id      = aws_vpc.vpc.id
+  port        = 8080
+  protocol    = "HTTP"
   target_type = "ip"
 
   health_check {
-    path = "/health"
+    path    = "/health"
     matcher = "200"
   }
 }
