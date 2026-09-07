fa99a52bec5a5a49d045a6e0fff57ed757b2910b
puneeth-grinds <puneethm2736@gmail.com>
2026-09-07 07:21:28 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -5,3 +5,16 @@ resource "aws_lb" "lb" {
   subnets            = [aws_subnet.public_1a, aws_subnet.private_1b]
   security_groups    = [aws_security_group.alb_sg.id]
 }
+
+resource "aws_alb_target_group" "api_alb_target" {
+  name = "api-gateway-target-group"
+  vpc_id = aws_vpc.vpc.id
+  port = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+
+  health_check {
+    path = "/health"
+    matcher = "200"
+  }
+}
