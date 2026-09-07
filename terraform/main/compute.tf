5f5108fd63fb6eaf1681a9132a5c6b860eb6fe64
puneeth-grinds <puneethm2736@gmail.com>
2026-09-07 08:42:37 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -169,4 +169,9 @@ resource "aws_ecs_service" "ecs_service_apigateway" {
   desired_count   = 1
   launch_type     = "FARGATE"
   depends_on      = [aws_lb_listener.lb_listener]
+  network_configuration {
+    subnets = [ aws_subnet.private_1a.id, aws_subnet.private_1b.id ]
+    security_groups = [ aws_security_group.ecs_sg ]
+    assign_public_ip = false
+  }
 }
