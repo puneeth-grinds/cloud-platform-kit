c2c9a741e0f6c565a8eac27a5012c5851af6deaf
puneeth-grinds <puneethm2736@gmail.com>
2026-09-07 08:39:29 +0530

    readme

--- terraform/main/compute.tf
+++ terraform/main/compute.tf
@@ -161,3 +161,11 @@ resource "aws_ecs_task_definition" "ecs_task_vulscanner" {
   ])
 
 }
+
+resource "aws_ecs_service" "ecs_service_apigateway" {
+  name = "api-gateway"
+  cluster = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_apigateway.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+}
