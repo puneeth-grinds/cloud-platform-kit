828e916c49d41c578f8e440280c4238b710c8813
puneeth-grinds <puneethm2736@gmail.com>
2026-09-08 11:10:46 +0530

    api-gateway

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"github.com/puneeth-grinds/cloud-platform-kit/services/api-gateway/internal/config"
+	"net/http"
 )
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
 
 func main() {
 	_, err := config.Load()
