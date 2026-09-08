dbfba9cec4663a882630652d439e8271035be6c8
puneeth-grinds <puneethm2736@gmail.com>
2026-09-08 10:42:30 +0530

    api-gateway

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -1,5 +1,15 @@
-package main 
+package main
 
-func main(){
-	
+import (
+	"errors"
+
+	"github.com/puneeth-grinds/cloud-platform-kit/services/api-gateway/internal/config"
+
+)
+
+func main() {
+	_, err := config.Load()
+	if err != nil {
+		errors.New("Config's are not loaded")
+	}
 }
--- services/api-gateway/internal/config/config.go
+++ services/api-gateway/internal/config/config.go
@@ -23,11 +23,11 @@ func getEnv(key, fallback string) string {
 }
 
 func Load ()(Config, error){
-	cfg := {
+	cfg := Config{
 		Port: getEnv("PORT", "8080"),
 		LogLevel: getEnv("LOG_LEVEL", "info"),
 		ScannerURL: os.Getenv("SCANNER_URL"),
-		APIKey: os.Getenv("API_KEY")
+		APIKey: os.Getenv("API_KEY"),
 	}
 	if cfg.ScannerURL == ""{
 		return Config{}, errors.New("SCANNER_URL is required")
@@ -35,4 +35,5 @@ func Load ()(Config, error){
 	if cfg.APIKey == ""{
 		return Config{}, errors.New("API_KEY is required")
 	}
+	return cfg, nil
 }
