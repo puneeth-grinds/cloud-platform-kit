14f708c9f333fd33277bd02d5ed9d6d4e93e1cba
puneeth-grinds <puneethm2736@gmail.com>
2026-09-08 10:29:42 +0530

    api-gateway

--- services/api-gateway/internal/config/config.go
+++ services/api-gateway/internal/config/config.go
@@ -1,8 +1,23 @@
 package config
 
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/tools/go/cfg"
+)
 type Config struct {
 	Port       string
 	LogLevel   string
-	ScannerUrl string
+	ScannerURL string
 	APIKey     string
+}
+
+func Load()(Config, error) {
+	cfg := Config(
+		Port: getEnv("PORT", "8080")
+		LogLevel: getEnv("Log_Level", "info")
+		ScannerURL: os.Getenv("Scanner_URL")
+		APIKey: os.Getenv("APIKEY")
+	)
 }
