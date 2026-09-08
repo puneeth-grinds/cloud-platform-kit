4f9866ed519f8cc97996f4d4fd93d0501546fed0
puneeth-grinds <puneethm2736@gmail.com>
2026-09-08 10:51:32 +0530

    api-gateway

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"errors"
-
 	"github.com/puneeth-grinds/cloud-platform-kit/services/api-gateway/internal/config"
-
 )
 
 func main() {
 	_, err := config.Load()
 	if err != nil {
-		errors.New("Config's are not loaded")
+		panic(err)
 	}
 }
