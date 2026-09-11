a0da963fd2b35c826bd7a07d702aec59f5b20e6f
puneeth-grinds <puneethm2736@gmail.com>
2026-09-11 10:25:39 +0530

    GoLang

--- services/api-gateway/internal/middleware/auth.go
+++ services/api-gateway/internal/middleware/auth.go
@@ -1,30 +1,31 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 type APIError struct {
 	Error string `json:"error`
-	Code  int `json:"code"`
+	Code  int    `json:"code"`
 }
-
 
 func APIKeyMiddleware(APIKey string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			apiKey := r.Header.Get("X-API-Key")
-			w.Header().Set("Content-Type", "application/json")
-
-			apierror := APIError{
-				Error: http.Error(http.StatusUnauthorized),
-				Code: http.StatusUnauthorized,
-			}
 
 			if apiKey == "" || apiKey != APIKey {
-				return 
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusUnauthorized)
+
+				apiError := APIError{
+					Error: "Invalid or missing API Key",
+					Code:  http.StatusUnauthorized,
+				}
+				json.NewEncoder(w).Encode(apiError)
 			}
-			next.ServeHTTP(w, r)
+
 		})
 	}
 }
