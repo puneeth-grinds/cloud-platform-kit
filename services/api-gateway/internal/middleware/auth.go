026404dc4938c5c9f7742a936fc99b09acc0ab69
puneeth-grinds <puneethm2736@gmail.com>
2026-09-11 10:16:34 +0530

    GoLang

--- services/api-gateway/internal/middleware/auth.go
+++ services/api-gateway/internal/middleware/auth.go
@@ -4,11 +4,16 @@ import (
 	"net/http"
 )
 
+type APIError struct {
+	Error string `json:"error`
+	Code  string `json:"code"`
+}
+
 func APIKeyMiddleware(APIKey string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			apiKey := r.Header.Get("X-API-Key")
-			w.Header().Set("Content-Type","application/json")
+			w.Header().Set("Content-Type", "application/json")
 
 			if apiKey == "" || apiKey != APIKey {
 				http.Error(w, "unauthorized", http.StatusUnauthorized)
