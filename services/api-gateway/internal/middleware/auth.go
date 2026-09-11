03fe974cef5dc928b04f4a89bedb81ebd2f215db
puneeth-grinds <puneethm2736@gmail.com>
2026-09-11 08:33:19 +0530

    GoLang

--- services/api-gateway/internal/middleware/auth.go
+++ services/api-gateway/internal/middleware/auth.go
@@ -1,2 +1,19 @@
 package middleware
 
+import (
+	"net/http"
+)
+
+func APIKeyMiddleware(APIKey string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			apiKey := r.Header.Get("X-API-KEY")
+
+			if apiKey == "" || apiKey != APIKey {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
