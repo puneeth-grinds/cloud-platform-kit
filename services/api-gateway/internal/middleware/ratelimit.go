d93fc41110787a449185556a897b80a812e1a924
puneeth-grinds <puneethm2736@gmail.com>
2026-09-12 10:02:31 +0530

    GoLang

--- services/api-gateway/internal/middleware/ratelimit.go
+++ services/api-gateway/internal/middleware/ratelimit.go
@@ -1,10 +1,17 @@
 package middleware
 
 import (
+	"sync"
 	"time"
 )
 
 type rateLimitingEntry struct {
 	Count       int
 	WindowStart time.Time
+}
+
+type RateLimiter struct{
+	requests sync.Map
+	rate int //maximum requests allowed
+	per time.Duration 
 }
