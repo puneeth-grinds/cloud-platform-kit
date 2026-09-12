527c56bcfdd1b60567b16846b3263e6c1eb8ded0
puneeth-grinds <puneethm2736@gmail.com>
2026-09-12 10:43:07 +0530

    GoLang

--- services/api-gateway/internal/middleware/ratelimit.go
+++ services/api-gateway/internal/middleware/ratelimit.go
@@ -28,15 +28,14 @@ func (rl *RateLimiter) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apiKey := r.Header.Get("X-API-Key")
 
-		val, found := rl.requests.Load(apiKey)
-		if !found {
-			entry := &rateLimitingEntry{
-				Count:       0,
-				WindowStart: time.Now(),
-			}
-			actual, _ := rl.requests.LoadOrStore(apiKey, entry)
-			entry = actual.(*rateLimitingEntry)
+		entry := &rateLimitingEntry{
+			Count: 0,
+			WindowStart: time.Now(),
 		}
+		actual, _ := rl.requests.LoadOrStore(apiKey, entry)
+		entry = actual.(*rateLimitingEntry)
+
+
 
 		next.ServeHTTP(w, r)
 
