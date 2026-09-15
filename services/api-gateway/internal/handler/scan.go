bc3b8e70aa8f7e0ccb0628ae012a130eba78f321
puneeth-grinds <puneethm2736@gmail.com>
2026-09-15 11:20:49 +0530

    GoLang

---
+++ services/api-gateway/internal/handler/scan.go
@@ -0,0 +1,12 @@
+package handler
+import(
+	"net/http"
+
+	"github.com/puneeth-grinds/cloud-platform-kit/services/api-gateway/internal/proxy"
+)
+
+func newScanHandler(scannerProxy *proxy.ScannerProxy) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		scanHandler(w, r, scannerProxy)
+	})
+}
