17bedf9bdd3f5549a697e5f9bd87f718a92e9840
puneeth-grinds <puneethm2736@gmail.com>
2026-09-15 07:42:11 +0530

    GoLang

--- services/api-gateway/internal/proxy/scanner.go
+++ services/api-gateway/internal/proxy/scanner.go
@@ -1,8 +1,10 @@
 package proxy
+
 import (
 	"net/http"
 )
+
 type ScannerProxy struct {
-	baseURL string `json:"baseurl"`
-	httpClient *http.Client  
+	baseURL    string `json:"baseurl"`
+	httpClient *http.Client
 }
